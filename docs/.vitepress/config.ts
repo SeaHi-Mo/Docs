f7124b2828ae6f8fde5a3fae9906e4521e060f74
Seahi <seahi-mo@foxmail.com>
2026-01-06 15:45:23 +0800

    feat: 更新侧边栏头像背景图片配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -66,7 +66,7 @@ const teekConfig = defineTeekConfig({
     shape: "circle-rotate", // 头像风格：square 为方形头像，circle 为圆形头像，circle-rotate 可支持鼠标悬停旋转
     name: "SeaHi", // 侧边栏个人昵称
     slogan: "言念君子，温其如玉", // 侧边栏个人座右铭
-    circleBgImg: "https://img.onedayxyy.cn/images/Teek/TeekBg/14.webp", // 侧边栏个人头像圆形背景图
+    circleBgImg: "/img/1.webp", // 侧边栏个人头像圆形背景图
     circleBgMask: false, // 头像圆形背景图是否显示遮罩层
     color: "#fff",
 
